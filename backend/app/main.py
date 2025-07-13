a310b863d502db017a531cdfe5a46fec7b0dd0f0
tomvannoord63 <thomaspvannoord@gmail.com>
2025-07-13 14:39:14 -0400

    fixing CORS policy error for docker backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://localhost:5173",  # Vite default port
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "http://0.0.0.0:5173",    # Docker container origin
     ],
     allow_credentials=True,
     allow_methods=["*"],

0b15f0a30accb32f54b751d66fc6f0d97410bfde
tomvannoord63 <thomaspvannoord@gmail.com>
2025-07-13 17:00:11 -0400

    adding static dockerfile

---
+++ deployment/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# LaserBot Deployment Script for ARMv6l Raspberry Pi
+# Builds frontend locally and deploys to Pi
+
+set -e
+
+PI_HOST="${1:-raspberrypi.local}"
+PI_USER="${2:-bot}"
+PROJECT_PATH="/home/$PI_USER/LaserBot"
+
+echo "Building frontend locally..."
+cd frontend
+npm run build
+
+echo "Copying built files to Pi..."
+rsync -avz --delete dist/ "$PI_USER@$PI_HOST:$PROJECT_PATH/frontend/dist/"
+
+echo "Copying deployment files to Pi..."
+rsync -avz deployment/ "$PI_USER@$PI_HOST:$PROJECT_PATH/deployment/"
+rsync -avz frontend/Dockerfile.static "$PI_USER@$PI_HOST:$PROJECT_PATH/frontend/"
+rsync -avz frontend/nginx.conf "$PI_USER@$PI_HOST:$PROJECT_PATH/frontend/"
+
+echo "Building and starting services on Pi..."
+ssh "$PI_USER@$PI_HOST" "cd $PROJECT_PATH && docker compose -f deployment/docker-compose.prod.yml up -d --build"
+
+echo "Deployment complete!"
+echo "Frontend: http://$PI_HOST:3000"
+echo "Backend: http://$PI_HOST:8000"
